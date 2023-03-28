425688504618a868708a2323c2a5207dc77682af
demidmalyanov <dagdog2506@gmail.com>
2023-03-28 14:07:30 +0500

    feat: add int to roman conversion

--- roman.go
+++ roman.go
@@ -2,6 +2,7 @@ package roman
 
 import (
 	"math"
+	"strconv"
 	"strings"
 )
 
@@ -71,4 +72,41 @@ func compareTwoRomans(roman1, roman2 string) int {
 		return -1
 	}
 
+}
+
+// integerToRoman converts int to roman numeral.
+func intToRoman(number int) string {
+	maxRomanNumber := 3999
+	if number > maxRomanNumber {
+		return strconv.Itoa(number)
+	}
+
+	conversions := []struct {
+		value int
+		digit string
+	}{
+		{1000, "M"},
+		{900, "CM"},
+		{500, "D"},
+		{400, "CD"},
+		{100, "C"},
+		{90, "XC"},
+		{50, "L"},
+		{40, "XL"},
+		{10, "X"},
+		{9, "IX"},
+		{5, "V"},
+		{4, "IV"},
+		{1, "I"},
+	}
+
+	var roman strings.Builder
+	for _, conversion := range conversions {
+		for number >= conversion.value {
+			roman.WriteString(conversion.digit)
+			number -= conversion.value
+		}
+	}
+
+	return roman.String()
 }
